160de2dcc004ece142f0c9cb2f5cc66b6a77338b
Lee Edwards <leeredwards@gmail.com>
2022-10-23 17:33:57 -0700

    Make RSVP fieldset names not editable

--- components/RsvpFields.js
+++ components/RsvpFields.js
@@ -8,7 +8,7 @@ export default function RsvpFields({ user, index }) {
   return (
     <fieldset className={styles.fieldset}>
       <input name={`id${suffix}`} type="hidden" value={user.id} />
-      <TextField name={`Name${suffix}`} label="Name" defaultValue={user.name} hideLabel hero />
+      <h3>{user.name}</h3>
       <TextField name={`Email${suffix}`} label="Email" defaultValue={user.email} />
       <TextField name={`Address${suffix}`} label="Address" defaultValue={user.address} />
 
