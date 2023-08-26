a724383c23b2b87ec868924c8d6e5c2ff4295dbb
Lee Edwards <leeredwards@gmail.com>
2023-08-26 16:03:56 -0700

    disable rsvp and survey unless env var is set

--- pages/rsvp/index.js
+++ pages/rsvp/index.js
@@ -6,10 +6,16 @@ import Submit from '@components/Submit'
 
 import styles from '../../styles/rsvp.module.css'
 import { useRouter } from 'next/router'
+import { useEffect } from 'react';
 
 export default function RsvpHome() {
   const router = useRouter();
   const error = router.query.error;
+  let rsvpEnabled = false;
+
+  useEffect(() => {
+    rsvpEnabled = process.env.RSVP_ENABLED;
+  }, []);
 
   return (
     <>
@@ -17,20 +23,28 @@ export default function RsvpHome() {
         <title>RSVP :: Lee & Nicole Wedding 09.09.2023</title>
       </Head>
 
-      <h3>Find Your Invitation</h3>
-      <Form action="/api/guests/auth" method="get" error={error} className={styles.form}>
-        <TextField name="name" label="Name" placeholder="Firstname Lastname" hero />
-        <p className={styles.or}>&mdash;or&mdash;</p>
-        <TextField name="email" label="Email Address" placeholder="you@emailaddress.com" hero />
-        <input type="hidden" name="redirect" value="rsvp" />
-        <Submit id="auth" label="Look Me Up" />
-      </Form>
+      {rsvpEnabled ? <>
+        <h3>Find Your Invitation</h3>
+        <Form action="/api/guests/auth" method="get" error={error} className={styles.form}>
+          <TextField name="name" label="Name" placeholder="Firstname Lastname" hero />
+          <p className={styles.or}>&mdash;or&mdash;</p>
+          <TextField name="email" label="Email Address" placeholder="you@emailaddress.com" hero />
+          <input type="hidden" name="redirect" value="rsvp" />
+          <Submit id="auth" label="Look Me Up" />
+        </Form>
 
-      <p className={styles.footer}>
-        Having trouble? E-mail us at <a href="mailto:aloha@leenicolewedding.com">aloha@leenicolewedding.com</a>
-        <br /><br />
-        <a href="/#content">Return home</a> for more wedding information.
-      </p>
+        <p className={styles.footer}>
+          Having trouble? E-mail us at <a href="mailto:aloha@leenicolewedding.com">aloha@leenicolewedding.com</a>
+          <br /><br />
+          <a href="/#content">Return home</a> for more wedding information.
+        </p>
+      </> : <>
+        <h3>Thank you for your RSVP!</h3>
+        <p className={styles.footer}>
+          <a href="/#content">Return home</a> for more wedding information.
+        </p>
+      </>
+      }
     </>
   );
 };
--- pages/survey/index.js
+++ pages/survey/index.js
@@ -6,10 +6,16 @@ import Submit from '@components/Submit'
 
 import styles from '../../styles/rsvp.module.css'
 import { useRouter } from 'next/router'
+import { useEffect } from 'react'
 
 export default function SurveyHome() {
   const router = useRouter();
   const error = router.query.error;
+  let surveyEnabled = false;
+
+  useEffect(() => {
+    surveyEnabled = process.env.SURVEY_ENABLED;
+  }, []);
 
   return (
     <>
@@ -17,18 +23,26 @@ export default function SurveyHome() {
         <title>Guest Survey :: Lee & Nicole Wedding 09.09.2023</title>
       </Head>
 
-      <h3>Look Yourself Up</h3>
-      <Form action="/api/guests/auth" method="get" error={error} className={styles.form}>
-        <TextField name="name" label="Name" placeholder="Firstname Lastname" hero />
-        <input type="hidden" name="redirect" value="survey" />
-        <Submit id="auth" label="Look Me Up" />
-      </Form>
+      {surveyEnabled ? <>
+        <h3>Look Yourself Up</h3>
+        <Form action="/api/guests/auth" method="get" error={error} className={styles.form}>
+          <TextField name="name" label="Name" placeholder="Firstname Lastname" hero />
+          <input type="hidden" name="redirect" value="survey" />
+          <Submit id="auth" label="Look Me Up" />
+        </Form>
 
-      <p className={styles.footer}>
-        Having trouble? E-mail or text us!
-        <br /><br />
-        <a href="/#content">Return home</a> for more wedding information.
-      </p>
+        <p className={styles.footer}>
+          Having trouble? E-mail or text us!
+          <br /><br />
+          <a href="/#content">Return home</a> for more wedding information.
+        </p>
+      </> : <>
+        <h3>Thank you for your guest survey response!</h3>
+        <p className={styles.footer}>
+          <a href="/#content">Return home</a> for more wedding information.
+        </p>
+      </>
+      }
     </>
   );
 };
